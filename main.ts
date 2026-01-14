36d19c0bb9fcedb6bed7c4a32ba40f09d247d84b
Aarav Singhal <aarav.singhal@stu.ocsb.ca>
2026-01-14 12:33:02 -0500

    it works

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Aarav Singhal AND Max Malik
+ * Created on: Jan 2026
+ * This program moves 2 servos and one stepper motor.
 */
 
-basic.showString('Hello, World!')
+let potentiometers1 = 0
+let potentiometers2 = 0
+let angle1 = 0
+let angle2 = 0
+
+basic.forever(function () {
+    // Read potentiometers (0–1023)
+    potentiometers1 = pins.analogReadPin(AnalogPin.P1)
+    potentiometers2 = pins.analogReadPin(AnalogPin.P2)
+    // Map to servo angles (0–180)
+    angle1 = Math.map(potentiometers1, 0, 1023, 0, 180)
+    angle2 = Math.map(potentiometers2, 0, 1023, 0, 180)
+    // Move servos
+    robotbit.Servo(robotbit.Servos.S1, angle1)
+    robotbit.Servo(robotbit.Servos.S2, angle2)
+    basic.pause(20)
+})
+
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.A)) {
+        robotbit.StpCarMove(1, 48)
+    }
+})
+
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.B)) {
+        robotbit.StpCarMove(-1, 48)
+    }
+})
